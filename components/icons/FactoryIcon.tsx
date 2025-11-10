af6c60f0e12cf9c0ba672419191dea9628f9cbe4
AbouElNour <aboelnour1212@gmail.com>
2025-11-11 01:40:41 +0300

    Updated backend files to fix API handling

---
+++ components/icons/FactoryIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const FactoryIcon: React.FC = () => (
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        className="h-4 w-4 text-gray-400 dark:text-slate-500 flex-shrink-0" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+    >
+        <path d="M2 20a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8l-7 5V8l-7 5V4a2 2 0 0 0-2-2H4a2 2 0 0 0-2 2Z"/>
+    </svg>
+);
+
+export default FactoryIcon;
